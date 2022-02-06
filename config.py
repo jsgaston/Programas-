d058fd8d2f1ba7321862be447061b959ef4090ae
jsgaston <69969971+jsgaston@users.noreply.github.com>
2022-02-06 23:05:18 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ Created on Fri Nov 26 23:31:05 2021
 @author: jsgas
 """
 
-API_KEY = '9uztM6LLWdyE5Go10LNhec1RNriHuSAR6YJhn44b6FhVUFe0Ik6sPWAGLC9DBaH7'
-API_SECRET = 'Kyitq0Kpjkwygz2q2QWiubt5OZsSd2fKEvG3G3gBmWScY2STVhc22rIaMbv10kUY'
+API_KEY = ''
+API_SECRET = ''
