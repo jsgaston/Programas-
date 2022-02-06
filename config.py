948b8614525b6e84fb4edc4275732892f1e7a3fa
jsgaston <69969971+jsgaston@users.noreply.github.com>
2022-02-06 22:59:33 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 26 23:31:05 2021
+
+@author: jsgas
+"""
+
+API_KEY = '9uztM6LLWdyE5Go10LNhec1RNriHuSAR6YJhn44b6FhVUFe0Ik6sPWAGLC9DBaH7'
+API_SECRET = 'Kyitq0Kpjkwygz2q2QWiubt5OZsSd2fKEvG3G3gBmWScY2STVhc22rIaMbv10kUY'
